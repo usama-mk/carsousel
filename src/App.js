9761f59855090573d0991bd5a4ec0fbc581b3ddd
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-10-07 18:45:07 +0500

    Admin Panel added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import DemoCarousel from './Pages/carousel';
+import Login from './Pages/Login';
+
 
 function App() {
   return (
+
     <div className="App">
-     <DemoCarousel/>
+
+      <Switch>
+      <Route exact path='/carsousel' render={()=>(<DemoCarousel adminCheckP={false} />)}  />
+     <Route exact path='/admin' component={Login}  />
+     <Route exact path='/admin/carsousel' component={DemoCarousel}/>
+    
+     </Switch>
+    
     </div>
   );
 }
--- src/Pages/carousel.jsx
+++ src/Pages/carousel.jsx
@@ -1,21 +1,29 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './carousel.css';
 import { Button, IconButton } from '@material-ui/core';
-import { Delete, CloudUpload } from "@material-ui/icons";
+import { Delete, CloudUpload, VerifiedUser, Home } from "@material-ui/icons";
 import { useState } from "react";
 import { storage } from "../firebase";
 import { db } from "../firebase";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { adminCheck } from "./Login";
 var React = require('react');
 var Carousel = require('react-responsive-carousel').Carousel;
 
 
-const DemoCarousel = () => {
+const DemoCarousel = (props) => {
+
 
     const form = "https://goasolutions.paperform.co/";
     const [image, setImage] = useState(null);
     const [urlArray, setUrlArray] = useState([]);
     const [progress, setProgress] = useState(0);
+    const [adminCheck, setAdminCheck] = useState(props.adminCheckP);
+    console.log("admin pppp check: ")
+    console.log(props.adminCheckP);
+
+
 
     useEffect(() => {
         db.collection("imagesData").onSnapshot((snapshot) =>
@@ -38,46 +46,50 @@ const DemoCarousel = () => {
     const deleteImage = (id, name) => {
         db.collection("imagesData").doc(id).delete();
         //delete from storage
-        var desertRef = storage.ref(`images/${image.name}`);
-        desertRef.delete().then(function() {
-            // File deleted successfully
-          }).catch(function(error) {
-            // Uh-oh, an error occurred!
-            console.log(error);
-          });
+
+
+        // var desertRef = storage.ref(`images/${image.name}`);
+        // desertRef.delete().then(function() {
+        //     // File deleted successfully
+        //   }).catch(function(error) {
+        //     // Uh-oh, an error occurred!
+        //     console.log(error);
+        //   });
     }
 
     const uploadFileHandler = () => {
-        if(image){
-        const uploadTask = storage.ref(`images/${image.name}`).put(image);
-        uploadTask.on(
-            "state_changed",
-            snapshot => {
-                const progress = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
-                setProgress(progress);
-            },
-            error => {
-                console.log(error);
-            },
-            () => {
-                storage.ref("images").child(image.name).getDownloadURL().then(url => {
-                    urlArray.push(url);
-                    console.log(urlArray);
-                    db.collection("imagesData").add({
-                        imageUrl: url,
-                        imageName: image.name
-                    });
+        if (image) {
+            const uploadTask = storage.ref(`images/${image.name}`).put(image);
+            uploadTask.on(
+                "state_changed",
+                snapshot => {
+                    const progress = Math.round(
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    );
+                    setProgress(progress);
+                },
+                error => {
+                    console.log(error);
+                },
+                () => {
+                    storage.ref("images").child(image.name).getDownloadURL().then(url => {
+                        urlArray.push(url);
+                        console.log(urlArray);
+                        db.collection("imagesData").add({
+                            imageUrl: url,
+                            imageName: image.name
+                        });
 
-                })
-            }
-        )
+                    })
+                }
+            )
+        }
     }
-}
+
+
 
     return (
-        <div className="carousel">
+        true ? <div className="carousel">
 
             <Carousel autoPlay interval="2000" infiniteLoop swipeable emulateTouch dynamicHeight showThumbs={false} >
 
@@ -85,11 +97,11 @@ const DemoCarousel = () => {
                     (<div>
 
                         <IconButton href={form} style={{ width: "99vw" }}>
-                            <img  alt="Not Loaded" src={url.data.imageUrl} height="400" width="480" />
+                            <img alt="Not Loaded" src={url.data.imageUrl} height="400" width="480" />
 
                         </IconButton>
                         <br />
-                        <Button
+                        {props.adminCheckP ? (<Button
                             onClick={() => { deleteImage(url.id, url.data.imageName) }}
                             variant="contained"
                             color="default"
@@ -98,7 +110,7 @@ const DemoCarousel = () => {
                             style={{ margin: "0", position: "relative", bottom: "40px" }}
                         >
                             Delete
-    </Button>
+                        </Button>) : <div> </div>}
 
                     </div>)
                 )}
@@ -108,24 +120,58 @@ const DemoCarousel = () => {
                     <p className="legend">Legend 14</p>
                 </div> */}
             </Carousel>
-            <div className="carousel_actions">
+            {props.adminCheckP ? (<div className="carousel_actions">
 
                 <progress value={progress} max="100" />
                 <br />
-                <input type="file" onChange={selectFileHandler}id="upload-button" />
+                <input type="file" onChange={selectFileHandler} id="upload-button" />
 
                 <Button
                     onClick={uploadFileHandler}
                     variant="contained"
-                    color="default"
+                    color="primary"
                     // className="uploadButton"
                     startIcon={<CloudUpload />}
                 >
                     Upload
-    </Button>
+</Button>
 
-            </div>
-        </div>
+
+
+                <a href="/carsousel">
+
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        // className="uploadButton"
+                        startIcon={<Home />}
+                    >
+                        Home
+</Button>
+                </a>
+
+
+
+
+
+
+            </div>) : (<div>
+                <a href={"/admin"}>
+
+                    <Button
+                        // onClick={adminHandler}
+                        variant="contained"
+                        color="primary"
+                        // className="uploadButton"
+                        startIcon={<VerifiedUser />}
+                    >
+                        Admin Login
+</Button>
+                </a>
+
+            </div>)}
+
+        </div> : <div><h1>hi</h1></div>
 
     );
 
